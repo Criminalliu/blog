4c561965f1ae7dd9fc62102bcad8b19b7f86d5aa
949721965 <55116171+949721965@users.noreply.github.com>
2020-05-31 22:19:36 +0800

    创建gradle项目

---
+++ src/main/java/com/blog/yinong/YinongApplication.java
@@ -0,0 +1,13 @@
+package com.blog.yinong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YinongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YinongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/yinong/YinongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.yinong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YinongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
