339a9cc84704870a9d0186b76f01d2c91bb9f0ff
949721965 <55116171+949721965@users.noreply.github.com>
2020-09-16 21:34:16 +0800

    成功使用注解

--- src/main/java/com/blog/yinong/controller/HelloController.java
+++ src/main/java/com/blog/yinong/controller/HelloController.java
@@ -1,15 +1,27 @@
 package com.blog.yinong.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-//@Controller
-@RestController
+@Controller
+//@RestController
 public class HelloController {
+
+    @Autowired
+    private yinong yinong;
+
+
+    @Value("${yinong.name}")
+    private String name;
+
+
     @RequestMapping("/hello")
-    public String hello(){
-        return "denglu";
+    public @ResponseBody
+    int hello(){
+        return yinong.getAge();
 //        System.out.println("nihao");st
     }
 }
